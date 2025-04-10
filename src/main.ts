aabce5bb4841346865b65af8d3f8d24e1049599a
soultone <soultonew2@gmail.com>
2025-04-10 22:42:55 +0100

    feat: drag and drop card

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@
 
 import './assets/main.css'
 import Modal from '@/components/Modal.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -9,10 +12,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+library.add([faPencilAlt, faTrashAlt])
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.component('Modal', Modal)
 
--- src/views/board/Overview.vue
+++ src/views/board/Overview.vue
@@ -2,57 +2,93 @@
   <div class="w-11/12 mx-auto mt-[2rem] font-poppins overflow-x-auto">
     <div class="font-semibold mb-3">Board 1</div>
     <div class="flex h-[calc(100vh-10rem)] space-x-3">
-      <div
-        v-for="(item, i) in list"
-        :key="item.id"
-        class="bg-white h-full w-[20%] overflow-y-scroll rounded-md p-2 flex-shrink-0"
+      <VueDraggable
+        @add="onAdd"
+        :group="{ name: 'cards', pull: true, put: true }"
+        class="w-full flex"
+        v-model="list"
       >
-        <div class="flex justify-between">
-          <input
-            type="text"
-            :key="'input-' + item.id"
-            v-model="item.name"
-            @focus="onFocus"
-            @blur="onBlur"
-            :class="listInputClassName"
-          />
-          <div class="flex items-center">
-            <button
-              class="text-xl font-medium cursor-pointer hover:bg-gray-200 w-8 rounded-md"
-              @click="addCard(i)"
-            >
-              +
-            </button>
-            <button class="text-2xl font-medium cursor-pointer hover:bg-gray-200 w-8 rounded-md">
-              <sup>...</sup>
-            </button>
-          </div>
-        </div>
-
-        <hr />
-
         <div
-          v-for="card in item.cards"
-          :key="card.id"
-          class="w-full bg-gray-200 my-4 rounded-md min-h-[8rem] max-h-[14rem]"
+          v-for="(item, i) in list"
+          :key="item.id"
+          class="bg-white h-full w-[20%] overflow-y-scroll rounded-md p-2 flex-shrink-0"
         >
-          A
+          <div class="flex justify-between">
+            <input
+              type="text"
+              :key="'input-' + item.id"
+              v-model="item.name"
+              @focus="onFocus"
+              @blur="onBlur"
+              :class="listInputClassName"
+            />
+            <div class="flex items-center">
+              <button
+                class="text-xl font-medium cursor-pointer hover:bg-gray-200 w-8 rounded-md"
+                @click="addCard(i)"
+              >
+                +
+              </button>
+              <button class="text-2xl font-medium cursor-pointer hover:bg-gray-200 w-8 rounded-md">
+                <sup>...</sup>
+              </button>
+            </div>
+          </div>
+
+          <hr />
+          <VueDraggable
+            :group="{ name: 'cards', pull: true, put: true }"
+            itemKey="id"
+            :animation="200"
+            @change="onCardChange"
+          >
+            <div
+              v-for="(card, j) in item.cards"
+              :key="card.id"
+              class="w-full group bg-gray-200 my-4 p-2 rounded-md min-h-[8rem] cursor-pointer max-h-[14rem]"
+            >
+              <div class="flex justify-end h-8">
+                <div class="flex space-x-4">
+                  <div class="hidden group-hover:block">
+                    <font-awesome-icon icon="pencil-alt" class="cursor-pointer" />
+                  </div>
+                  <div class="hidden group-hover:block">
+                    <font-awesome-icon
+                      icon="trash-alt"
+                      class="cursor-pointer"
+                      @click="moveToTrash(i, j)"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <textarea
+                class="w-full resize-none bg-gray-100 outline-none p-2"
+                id=""
+                rows="4"
+                @blur="handleBlur"
+                v-model="card.content"
+              ></textarea>
+            </div>
+          </VueDraggable>
         </div>
-      </div>
-      <div class="w-[8rem] flex-shrink-0">
-        <button
-          class="bg-gray-300 cursor-pointer bg-opacity-10 p-2 rounded-md w-full"
-          @click="addNewList"
-        >
-          Nouvelle liste
-        </button>
-      </div>
+
+        <div class="w-[8rem] flex-shrink-0">
+          <button
+            class="bg-gray-300 cursor-pointer bg-opacity-10 p-2 rounded-md w-full"
+            @click="addNewList"
+          >
+            Nouvelle liste
+          </button>
+        </div>
+      </VueDraggable>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { VueDraggable } from 'vue-draggable-plus'
 
 const listInputClassName = ref('outline-none font-semibold w-[8ch]')
 
@@ -65,6 +101,9 @@ const list = ref([
   },
 ])
 
+const el = ref(null)
+const l = ref(null)
+
 function addNewList() {
   list.value.push({
     id: list.value.length + 1,
@@ -75,13 +114,30 @@ function addNewList() {
 }
 
 function addCard(index) {
-  list.value[index].cards.push({ id: list.value[index].cards.length + 1, content: '' })
+  list.value[index].cards.unshift({ id: list.value[index].cards.length + 1, content: '' })
+  console.log({ list })
+}
+
+function onAdd(event) {
+  console.log('add', event)
+}
+
+function moveToTrash(i, j) {
+  list.value[i].cards = list.value[i].cards.filter((el, ind) => ind !== j)
   console.log({ list })
 }
 
 function onBlur() {
   console.log({ list })
   listInputClassName.value = 'outline-none font-semibold w-[8ch]'
+}
+
+function handleBlur() {
+  console.log({ list })
+}
+
+function onCardChange(event) {
+  console.log('Card moved:', event)
 }
 
 function onFocus() {
