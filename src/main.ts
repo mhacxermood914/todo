37f01dc738f09f8f677518738d4222ff2ac51a83
soultone <soultonew2@gmail.com>
2025-04-10 13:27:33 +0100

    feat: auth pags setup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="font-poppins flex items-center justify-center h-screen bg-[#eeee]">
+    <div class="w-1/3 bg-white rounded-md shadow-sm p-4">
+      <div class="flex justify-between">
+        <div class="uppercase font-semibold">S'authentifier</div>
+        <router-link :to="{ name: 'register' }" class="text-sm underline text-blue-900"
+          >S'inscrire</router-link
+        >
+      </div>
+
+      <form class="space-y-3 my-4">
+        <div class="flex flex-col space-y-2">
+          <label for="">Email</label>
+          <input type="text" class="bg-[#eeee] outline-none h-[2rem] px-1" />
+        </div>
+        <div class="flex flex-col space-y-2">
+          <label for="">Password</label>
+          <input type="text" class="bg-[#eeee] outline-none h-[2rem] px-1" />
+        </div>
+        <div class="">
+          <button
+            class="bg-blue-900 p-2 text-white rounded-md mt-3 w-full hover:bg-white hover:text-black in-hover:border-blue-900 hover:border hover:cursor-pointer"
+          >
+            Se connecter
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
